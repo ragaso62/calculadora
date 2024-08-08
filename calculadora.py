3499d11b18854f12b26c0ad5ef6d4aa1b0154d31
lucas <lucas.r.s.s.2031@gmail.com>
2024-08-08 17:16:12 -0300

    add das quatos operaçoes e layout

---
+++ calculadora.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def adicionar():
+    try:
+        resultado.set(float(entrada1.get()) + float(entrada2.get()))
+    except ValueError:
+        messagebox.showerror("erro","por favor, insira numeros validos.")
+
+def subitrair():
+    try:
+        resultado.set(float(entrada1.get()) - float(entrada2.get()))
+    except ValueError:
+        messagebox.showerror('erro','por favor, insira numeros validos.')
+
+def multiplicacao():
+    try:
+        resultado.set(float(entrada1.get()) * float(entrada2.get()))
+    except ValueError:
+        messagebox.showerror('erro','por favor, insira numeros validos.')
+
+def divisao():
+    try:
+        if float(entrada2.get()) == 0:
+            raise ZeroDivisionError
+        resultado.set(float(entrada1.get()) / float(entrada2.get()))
+    except ValueError:
+        messagebox.showerror('erro', 'por favor, insira numeros validos.')
+    except ZeroDivisionError:
+        messagebox.showerror('erro','divisão por zero não é permitido')
+
+
+
+root = tk.Tk()
+
+root.title('calculadora hackeada')
+
+entrada1 = tk.StringVar()
+entrada2 = tk.StringVar()
+resultado = tk.StringVar()
+
+tk.Label(root,text='primeiro numero: ').grid(row=0, column=0)
+tk.Entry(root,textvariable=entrada1).grid(row=0, column=1)
+
+
+tk.Label(root,text='segundo numero: ').grid(row=1, column=0)
+tk.Entry(root,textvariable=entrada2).grid(row=1, column=1)
+
+tk.Button(root,text='+', command=adicionar).grid(row=2, column=0)
+tk.Button(root,text='-', command=subitrair).grid(row=2, column=1)
+tk.Button(root,text='x', command=multiplicacao).grid(row=2, column=2)
+tk.Button(root,text=':', command=divisao).grid(row=2, column=3)
+
+tk.Label(root, text='resultado: ').grid(row=3,column=0)
+tk.Entry(root, textvariable=resultado, state='readonly').grid(row=3, column=1)
+
+root.mainloop()
